6859ae9ae473913ae0906d12e42fcad21c936d16
Edoardo Ranghieri <edoardo@ranghieri.com>
2021-08-23 19:41:31 +0200

    Cleanup code

--- init.lua
+++ init.lua
@@ -66,6 +66,22 @@ vim.o.scrolloff = 7
 vim.o.sidescrolloff = 14
 vim.o.sidescroll = 5
 vim.g.python_recommended_style = false
+vim.g.vscode_style = 'dark'
+vim.cmd('colorscheme vscode')
+vim.g.highlightedyank_highlight_duration = 300
+vim.g.mkdp_port = 42069
+
+-- Plugins setup
+require('nvim_comment').setup()
+require('nvim-autopairs').setup()
+require('colorizer').setup()
+require('lualine').setup{
+  options = {
+    theme = 'vscode',
+    section_separators = '',
+    component_separators = '|'
+  }
+}
 
 -- Autocommands
   -- Return to last position
@@ -78,49 +94,32 @@ vim.cmd(
 vim.cmd('autocmd VimEnter * setlocal formatoptions-=c formatoptions-=r formatoptions-=o')
 
 -- Keymaps
+local map = vim.api.nvim_set_keymap
 local keymap_opts = { noremap = true, silent = true }
 
   -- If char deleted with 'x', don't put it into clipboard
-vim.api.nvim_set_keymap('', 'x', '"_x', keymap_opts)
-vim.api.nvim_set_keymap('', 'X', '"_x', keymap_opts)
+map('', 'x', '"_x', keymap_opts)
+map('', 'X', '"_x', keymap_opts)
 
   -- Move lines up and down with alt
-vim.api.nvim_set_keymap('n', '<A-j>', ':m .+1<CR>==', keymap_opts)
-vim.api.nvim_set_keymap('n', '<A-k>', ':m .-2<CR>==', keymap_opts)
-vim.api.nvim_set_keymap('i', '<A-j>', '<Esc>:m .+1<CR>==gi', keymap_opts)
-vim.api.nvim_set_keymap('i', '<A-k>', '<Esc>:m .-2<CR>==gi', keymap_opts)
-vim.api.nvim_set_keymap('v', '<A-j>', ":m '>+1<CR>gv=gv", keymap_opts)
-vim.api.nvim_set_keymap('v', '<A-k>', ":m '<-2<CR>gv=gv", keymap_opts)
+map('n', '<A-j>', ':m .+1<CR>==', keymap_opts)
+map('n', '<A-k>', ':m .-2<CR>==', keymap_opts)
+map('i', '<A-j>', '<Esc>:m .+1<CR>==gi', keymap_opts)
+map('i', '<A-k>', '<Esc>:m .-2<CR>==gi', keymap_opts)
+map('v', '<A-j>', ":m '>+1<CR>gv=gv", keymap_opts)
+map('v', '<A-k>', ":m '<-2<CR>gv=gv", keymap_opts)
 
 -- Setup copy/paste from/to clipboard
   -- WSL platform check
 if string.find(vim.fn.system('uname -r'), 'microsoft') ~= nil then
-  vim.api.nvim_set_keymap('v', '<Leader>y', ":'<,'>w !clip.exe<CR><CR>", keymap_opts)
+  map('v', '<Leader>y', ":'<,'>w !clip.exe<CR><CR>", keymap_opts)
 else
-  vim.api.nvim_set_keymap('n', '<Leader>yy', '"+yy', keymap_opts)
-  vim.api.nvim_set_keymap('v', '<Leader>y', '"+y', keymap_opts)
-  vim.api.nvim_set_keymap('n', '<Leader>p', '"+p', keymap_opts)
-  vim.api.nvim_set_keymap('n', '<Leader>P', '"+P', keymap_opts)
+  map('n', '<Leader>yy', '"+yy', keymap_opts)
+  map('v', '<Leader>y', '"+y', keymap_opts)
+  map('n', '<Leader>p', '"+p', keymap_opts)
+  map('n', '<Leader>P', '"+P', keymap_opts)
 end
 
--- Plugins
-require('nvim_comment').setup()
-require('nvim-autopairs').setup()
-require('colorizer').setup()
-require('lualine').setup{
-  options = {
-    theme = 'vscode',
-    section_separators = '',
-    component_separators = '|'
-  }
-}
-
-vim.g.vscode_style = 'dark'
-vim.cmd('colorscheme vscode')
-
-vim.g.highlightedyank_highlight_duration = 300
-
-vim.g.mkdp_port = 42069
-vim.api.nvim_set_keymap('n', '<Leader>mp', ':MarkdownPreviewToggle<CR>', keymap_opts)
+map('n', '<Leader>mp', ':MarkdownPreviewToggle<CR>', keymap_opts)
 
 vim.cmd("command WW :execute ':SudaWrite'")
